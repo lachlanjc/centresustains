5907aebf0d1d7e0626accfa371f44566db95240b
Lachlan Campbell <lachlanjc@hey.com>
2020-12-15 10:58:23 -0500

    Add photos

--- pages/climate.js
+++ pages/climate.js
@@ -16,6 +16,26 @@ const ClimatePage = () => (
         </Text>
       </Container>
     </Box>
+    <Box as="section" bg="sunken">
+      <Box as="header" p={[3, 4, 5]}>
+        <Heading as="h2" variant="title" color="secondary">Roads</Heading>
+      </Box>
+      <Grid columns={[null, 3]} gap={[2, 3]}>
+        <Image src="/photos/roads_2.jpg" alt="Panorama of winding road around mountain, frame 1" width={2336} height={3504} />
+        <Image src="/photos/roads_1.jpg" alt="Panorama of winding road around mountain, frame 2" width={1912} height={2868} />
+        <Image src="/photos/roads_3.jpg" alt="Panorama of winding road around mountain, frame 3" width={2336} height={3504} />
+      </Grid>
+    </Box>
+    <Box as="section" bg="sunken">
+      <Box as="header" p={[3, 4, 5]}>
+        <Heading as="h2" variant="title" color="green">Forests</Heading>
+      </Box>
+      <Grid columns={[null, 3]} gap={[2, 3]}>
+        <Image src="/photos/forest_spring.jpg" alt="Panorama of winding road around mountain, frame 1" width={1912} height={2868} />
+        <Image src="/photos/forest_summer.jpg" alt="Panorama of winding road around mountain, frame 2" width={1912} height={2868} />
+        <Image src="/photos/forest_fall.jpg" alt="Panorama of winding road around mountain, frame 3" width={1912} height={2868} />
+      </Grid>
+    </Box>
   </>
 )
 
