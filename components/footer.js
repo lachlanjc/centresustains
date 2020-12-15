d6e6c3bbdc54ad61cc180201cf5f64b99c92cac3
Lachlan Campbell <lachlanjc@hey.com>
2020-12-15 01:18:03 +0000

    Fix yarn.lock

--- components/footer.js
+++ components/footer.js
@@ -1,34 +1,21 @@
 import { Box, Container } from 'theme-ui'
-import Flag from './flag'
 import Content from './footer.mdx'
 
 const Footer = () => (
   <Box
     as="footer"
     sx={{
-      py: 5,
+      py: 4,
       bg: 'sunken',
       color: 'secondary',
-      textAlign: 'center',
-      fontSize: 1,
-      position: 'relative'
     }}
   >
-    <Flag />
     <Container
       variant="narrow"
       sx={{
+      textAlign: 'center',
         p: { mt: 3 },
-        a: { color: 'primary' },
-        h2: {
-          fontFamily: 'heading',
-          letterSpacing: 'headline',
-          lineHeight: 'heading',
-          color: 'secondary',
-          fontSize: [3, 4],
-          mt: 0,
-          mb: -2
-        }
+        a: { color: 'primary' }
       }}
     >
       <Content />
