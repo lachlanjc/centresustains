8ff8b44f1d037afa585f31a3d77bc3d38c1dee9d
Lachlan Campbell <lachlanjc@hey.com>
2020-12-17 16:04:08 -0500

    Add additional sections to Climate page

--- pages/climate.js
+++ pages/climate.js
@@ -6,8 +6,9 @@ import BGImg from '../components/bg-img'
 const ClimatePage = () => (
   <>
     <Box as="header" sx={{ position: 'relative' }}>
-      <Container variant="copy" sx={{ py: 4, textAlign: 'center' }}>
-        <Heading as="h1" variant="title">
+      <BGImg src="/photos/fog.jpg" alt="Fog over a meadow with a sunlit tree" width={3072} height={2048} />
+      <Container variant="copy" sx={{ pt: [5, 6], pb: [4, 5], textAlign: 'center', color: 'white', textShadow: 'text' }}>
+        <Heading as="h1" variant="title" color="inherit">
           Climate in Central PA
         </Heading>
         <Text as="p" variant="lead">
@@ -16,7 +17,23 @@ const ClimatePage = () => (
         </Text>
       </Container>
     </Box>
+    <Box as="section">
+      <Box as="header" p={[3, 4, 5]}>
+        <Heading as="h2" variant="title" color="green">Forests</Heading>
+      </Box>
+      <Grid columns={[null, 3]} gap={[2, 3]}>
+        <Image src="/photos/forest_spring.jpg" alt="Panorama of winding road around mountain, frame 1" width={1912} height={2868} />
+        <Image src="/photos/forest_summer.jpg" alt="Panorama of winding road around mountain, frame 2" width={1912} height={2868} />
+        <Image src="/photos/forest_fall.jpg" alt="Panorama of winding road around mountain, frame 3" width={1912} height={2868} />
+      </Grid>
+    </Box>
     <Box as="section" bg="sunken">
+      <Box as="header" p={[3, 4, 5]}>
+        <Heading as="h2" variant="title" color="brown">Wildlife</Heading>
+      </Box>
+      <Image src="/photos/deer.jpg" alt="Deer crouching on the forest floor" width={3504} height={2336} />
+    </Box>
+    <Box as="section">
       <Box as="header" p={[3, 4, 5]}>
         <Heading as="h2" variant="title" color="secondary">Roads</Heading>
       </Box>
@@ -28,13 +45,24 @@ const ClimatePage = () => (
     </Box>
     <Box as="section" bg="sunken">
       <Box as="header" p={[3, 4, 5]}>
-        <Heading as="h2" variant="title" color="green">Forests</Heading>
+        <Heading as="h2" variant="title" color="yellow">Weather</Heading>
       </Box>
-      <Grid columns={[null, 3]} gap={[2, 3]}>
-        <Image src="/photos/forest_spring.jpg" alt="Panorama of winding road around mountain, frame 1" width={1912} height={2868} />
-        <Image src="/photos/forest_summer.jpg" alt="Panorama of winding road around mountain, frame 2" width={1912} height={2868} />
-        <Image src="/photos/forest_fall.jpg" alt="Panorama of winding road around mountain, frame 3" width={1912} height={2868} />
+      <Image src="/photos/fog.jpg" alt="Fog over a meadow with a sunlit tree" width={3072} height={2048} />
+    </Box>
+    <Box as="section">
+      <Box as="header" p={[3, 4, 5]}>
+        <Heading as="h2" variant="title" color="azure">Water</Heading>
+      </Box>
+      <Grid columns={2} gap={[2, 4]}>
+        <Image src="/photos/marsh.jpg" alt="Marsh with a small stream running through it" width={2048} height={3072} />
+        <Image src="/photos/water.jpg" alt="Lily pads with a flower on the surface of still water" width={2048} height={3072} />
       </Grid>
+    </Box>
+    <Box as="section" bg="sunken">
+      <Box as="header" p={[3, 4, 5]}>
+        <Heading as="h2" variant="title" color="purple">Farms</Heading>
+      </Box>
+      <Image src="/photos/farms.jpg" alt="Happy Valley Farm" width={3072} height={2048} />
     </Box>
   </>
 )
