fe40905bf16678d8d95addb2f8bd30f651716a45
Lachlan Campbell <lachlanjc@hey.com>
2020-12-17 17:07:50 -0500

    Update meta component

--- components/meta.js
+++ components/meta.js
@@ -1,50 +1,72 @@
 import Head from 'next/head'
 import theme from '../lib/theme'
 
+const makeTitle = (title, name) =>
+  title === name ? title : `${title} – ${name}`
+
 const Meta = ({
   title = 'Centre Sustains',
+  name = 'Centre Sustains',
   description = '',
   image = '',
-  url = 'https://centresustains.now.sh'
+  url = 'https://centresustains.now.sh',
+  children
 }) => (
     <Head>
-      <title>{title}</title>
-      <meta property="og:title" content={title} />
-      <meta name="twitter:title" content={title} />
-      <meta name="og:url" content={url} />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="COVID-19 Global Hackathon" />
-      <meta name="description" content={description} />
-      <meta property="og:description" content={description} />
-      <meta name="twitter:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content={image} />
+      <meta key="og_locale" property="og:locale" content="en_US" />
+      <meta key="og_type" property="og:type" content="website" />
+      <meta key="og_site" property="og:site_name" content={name} />
+      <title key="title">{makeTitle(title, name)}</title>
+      <meta key="og_title" property="og:title" content={makeTitle(title, name)} />
+      <meta
+        key="tw_title"
+        name="twitter:title"
+        content={makeTitle(title, name)}
+      />
+      {description && (
+        <>
+          <meta key="desc" name="description" content={description} />
+          <meta key="og_desc" property="og:description" content={description} />
+          <meta key="tw_desc" name="twitter:description" content={description} />
+        </>
+      )}
+      {image && (
+        <>
+          <meta key="og_img" property="og:image" content={image} />
+          <meta key="tw_card" name="twitter:card" content="summary_large_image" />
+          <meta key="tw_img" name="twitter:image" content={image} />
+        </>
+      )}
+      <meta key="theme_color" name="theme-color" content={theme.colors.primary} />
+      <meta key="tile_color" name="msapplication-TileColor" content={theme.colors.primary} />
       <link
+        key="safari_icon"
+        rel="mask-icon"
+        href={`${url}/safari-pinned-tab.png`}
+        color={theme.colors.primary}
+      />
+      <link
+        key="apple_icon"
         rel="apple-touch-icon"
         sizes="180x180"
         href={`${url}/apple-touch-icon.png`}
       />
       <link
+        key="favicon_32"
         rel="icon"
         type="image/png"
         sizes="32x32"
         href={`${url}/favicon-32x32.png`}
       />
       <link
+        key="favicon_16"
         rel="icon"
         type="image/png"
         sizes="16x16"
         href={`${url}/favicon-16x16.png`}
       />
-      <link rel="manifest" href={`${url}/site.webmanifest`} />
-      <link
-        rel="mask-icon"
-        href={`${url}/safari-pinned-tab.svg`}
-        color={theme.colors.primary}
-      />
-      <meta name="msapplication-TileColor" content={theme.colors.primary} />
-      <meta name="theme-color" content={theme.colors.primary} />
+      <link key="manifest" rel="manifest" href={`${url}/site.webmanifest`} />
+      {children}
     </Head>
   )
 
