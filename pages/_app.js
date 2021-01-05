a0e429c22deff1ed4f4c4417033136f6b611c4b9
Lachlan Campbell <lachlanjc@hey.com>
2021-01-05 16:29:19 -0500

    Add Fathom analytics code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import * as React from 'react'
-import NextApp from 'next/app'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 
 import '../public/fonts.css'
 import { ThemeProvider } from 'theme-ui'
@@ -8,16 +9,29 @@ import Meta from '../components/meta'
 import Nav from '../components/nav'
 import Footer from '../components/footer'
 
-export default class App extends NextApp {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider theme={theme}>
-        <Meta />
-        <Nav />
-        <Component {...pageProps} />
-        <Footer />
-      </ThemeProvider>
-    )
-  }
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load('TQSNNSCU', {
+      includedDomains: ['centresustains.com'],
+      url: 'https://crow.centresustains.com/script.js'
+    })
+    const onRouteChangeComplete = () => Fathom.trackPageview()
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Meta />
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
+  )
 }
+
+export default App
