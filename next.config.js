a0747524a1649dad2b776ba2ecc44cc2cd4a3d22
Lachlan Campbell <lachlanjc@hey.com>
2021-01-14 12:05:37 -0500

    Add card.jpg rewrite

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const withMDX = require('@next/mdx')()
-module.exports = withMDX({ pageExtensions: ['js', 'mdx'] })
+module.exports = withMDX({
+  pageExtensions: ['js', 'mdx'],
+  async rewrites() {
+    return [
+      {
+        source: '/card.jpg',
+        destination: '/cards/default.jpg'
+      }
+    ]
+  }
+})
