f6c2eee61014a53fa4b3ebf9c27f3fc7178b941a
Lachlan Campbell <lachlanjc@hey.com>
2021-02-20 22:28:36 -0500

    Track page completions

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as Fathom from 'fathom-client'
 
@@ -9,20 +9,57 @@ import Meta from '../components/meta'
 import Nav from '../components/nav'
 import Footer from '../components/footer'
 
+const scrollGoals = {
+  '/': 'A7NVRGKL',
+  '/forum': '62DPAVRO',
+  '/resources': 'UWQVJHND',
+  '/climate': 'AGN8URLH'
+}
+
 const App = ({ Component, pageProps }) => {
-  const router = useRouter()
+  const { events, pathname } = useRouter()
+  const [loggedPageScroll, setLoggedPageScroll] = useState(false)
 
   useEffect(() => {
     Fathom.load('TQSNNSCU', {
       includedDomains: ['centresustains.com'],
       url: 'https://crow.centresustains.com/script.js'
     })
-    const onRouteChangeComplete = () => Fathom.trackPageview()
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    const onRouteChangeComplete = () => {
+      Fathom.trackPageview()
+      setLoggedPageScroll(false)
+    }
+
+    events.on('routeChangeComplete', onRouteChangeComplete)
+
     return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
+      events.off('routeChangeComplete', onRouteChangeComplete)
     }
   }, [])
+
+  useEffect(() => {
+    const goal = scrollGoals[pathname]
+
+    const logPageEndReached = e => {
+      if (
+        goal &&
+        window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+        !loggedPageScroll
+      ) {
+        Fathom.trackGoal(goal, 0)
+        setLoggedPageScroll(true)
+      }
+    }
+
+    if (goal) {
+      window.addEventListener('scroll', logPageEndReached)
+
+      return () => {
+        window.removeEventListener('scroll', logPageEndReached)
+      }
+    }
+  }, [pathname])
 
   return (
     <ThemeProvider theme={theme}>
